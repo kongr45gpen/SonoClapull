73074bc08b0b1839407f8e701f8227dd9a83b7e4
kongr45gpen <electrovesta@gmail.com>
2017-08-06 01:17:44 +0300

    Add more calculations for frequencies
    Implements https://github.com/kongr45gpen/SonoClapper/issues/1

--- frequencyCalculator.cpp
+++ frequencyCalculator.cpp
@@ -12,38 +12,81 @@ int main() {
     double freq2 = 48000;
     double fftSize = 1024;
 
-    double minPeriod = 1/freq1;
-    double maxPeriod = 1/20.0;
+    double minPeriod = 1 / freq1;
+    double maxPeriod = 1 / 20.0;
 
     double tolerance = 2;
 
+    int testHarmonics = 10;
+    double hostileTolerance = freq2 / fftSize;
+
+    double tuning = 440.0;
+    double notes = 12;
+
     list<double> factors1;
     multimap<double, double> frequencies; // <error, frequency>
+    map<double, pair<double, double>> harmonics; // <harmonic, <frequency, factor>
+    multimap<double, pair<double, double>> hostileHarmonics; // <harmonic, <original frequency, factor>
 
-    for (int i = 0; i < fftSize/2.0; i++) {
+    cerr << "Hostile Harmonic Tolerance: " << hostileTolerance << " hz" << endl;
+
+    for (int i = 0; i < fftSize / 2.0; i++) {
         double nyquistFrequency = 0.5 * freq1;
-        double factor = i/(fftSize/2.0) * nyquistFrequency;
+        double factor = i / (fftSize / 2.0) * nyquistFrequency;
         factors1.push_back(factor);
     }
 
     std::cout.precision(10);
 
-    for (int i = 0; i < fftSize/2.0; i++) {
+    for (int i = 0; i < fftSize / 2.0; i++) {
         double nyquistFrequency = 0.5 * freq2;
-        double factor = i/(fftSize/2.0) * nyquistFrequency;
+        double factor = i / (fftSize / 2.0) * nyquistFrequency;
 
         for (double efactor : factors1) {
             if (fabs(efactor - factor) < tolerance) {
-                frequencies.emplace(fabs(efactor-factor), (factor+efactor)/2);
+                double average = (factor + efactor) / 2;
+                frequencies.emplace(fabs(efactor - factor), average);
+
+                // Add harmonics
+                for (int j = 2; j < testHarmonics + 2; j++) {
+                    harmonics.emplace(j * average, make_pair(average, j));
+                }
+
+                // Find hostile harmonics
+                for (pair<double, pair<double, double> > harmonic : harmonics) {
+                    if (fabs(harmonic.first - average) < hostileTolerance) {
+                        hostileHarmonics.emplace(average, harmonic.second);
+                    }
+                }
             }
         }
     }
 
     for (pair<double, double> freq : frequencies) {
+        // Calculate note
+        double note = notes * log2(freq.second / tuning);
+        double noteFrequency = tuning*pow(2, round(note)/notes);
+
         cout << "Found factor " << fixed << setprecision(5) << setw(12)
-                     << freq.second
-                     << " hz for frequency "
-                     << "(±"  << setw(5) << setprecision(3) << freq.first << ")"
-                     << endl;
+             << freq.second
+             << " hz for frequency "
+             << "(±" << setw(5) << setprecision(3) << freq.first << ")"
+             << " [" << setw(2) << hostileHarmonics.count(freq.second) << " hostile harmonics" << "]"
+                << " ♪" << setw(5) << setprecision(2) << note - round(note) << "±" << setw(7) << setprecision(3) << setfill('0') << fabs(freq.second - noteFrequency) << setfill(' ');
+        if (fabs(freq.second - noteFrequency) < hostileTolerance) {
+            // Announce when a note is too close
+            cout << "!!!";
+        }
+             cout << endl;
+    }
+
+    cout << "\n\n" << "Hostile harmonics:\n";
+    for (pair<double, pair<double, double> > harmonic : hostileHarmonics) {
+        cout << setprecision(3) << setw(9) << harmonic.first << " Hz = "
+             << setprecision(0) << setw(2) << harmonic.second.second
+             << " * " << setprecision(3) << setw(9) << harmonic.second.first
+             << " (±" << setw(6) << setprecision(3)
+             << fabs(harmonic.first - harmonic.second.second * harmonic.second.first) << ")"
+             << endl;
     }
 }
